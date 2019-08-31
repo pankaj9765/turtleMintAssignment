a17e058164423c4dd6ebaeb2abd262cc1361dc02
Pankaj Sanganwar <pankaj.cuelogic@nextseed.com>
2019-08-31 13:56:10 +0530

    removed commented code

--- programme.js
+++ programme.js
@@ -22,29 +22,6 @@ for(let i = 0; i < upperCaseString.length; i++) {
 }
 characters.sumOfCharacters = sum;
 
-// while(integer32Bit) {
-//   let currentSum = 0;
-//   let previousSum = 0;
-//   let currentCharacterCode = 65;
-//   let previousCharacterCode = 65;
-//   for(i = 1; i <= 32; i++) {
-//     currentSum = ( currentSum * 2) + i;
-//     previousSum = (previousSum * 2) + i;
-//     console.log(currentSum);
-//     console.log(previousSum);
-//   }
-// }
-
-var shortestString = {};
-
-let currentSum = 0;
-for(i = 1; i <= 32; i++) {
-  currentSum = ( currentSum * 2) + i;
-  shortestString[String.fromCharCode(64+i)] = currentSum;
-}
-console.log(shortestString);
-
 console.log('Character value of each charcter in string :', characters.eachCharacterValue);
 console.log('sum of characters value', characters.sumOfCharacters);
-console.log('shortest string: ', characters.shortestString);
 
